7fe4b97b5f5ee02089a621621097ee5e47022d23
Ahmed Adel <aa5707139@gmail.com>
2024-07-11 18:46:29 +0300

    Uploaded project 03

--- 01-Landing-Page/src/App.jsx
+++ 01-Landing-Page/src/App.jsx
@@ -15,24 +15,3 @@ function App() {
 }
 
 export default App;
-
-// Learned ///////////////////
-// importing images and css files is possible in jsx files
-// public vs src/assets folders for images
-// styles in css files are not restricted for a specific component, later on we will learn how to scope it
-// dynamic css
-// outputing list of data:           <ul>
-// {CORE_CONCEPTS.map((conceptItem) => (
-//   <CoreConcept {...conceptItem} />
-// ))}
-// </ul>
-
-///////////////////////
-// using js instead of jsx will require using React.createElement()... lec 61
-
-//setIsEditing((isEditing) => !isEditing); => change instantly
-//setIsEditing(!isEditing); => change is scheduled
-//<input type="text" required defaultValue={name} />
-
-// when setting the state of an object or an array, we make a new copy and set the value of the copy. because setting the value of the original will change it in the memory before react would execute it
-// minimize the number of states as much as possible and maximize the derived data from states
